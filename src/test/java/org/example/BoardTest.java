fc3afae37ba9c8b9c0c61ec06b3973d8c9d9c0b4
DziedzicGrzegorz <grzegorzdziedzic198@gmail.com>
2025-03-14 15:15:14 +0100

    test(Board): add collision detection tests for figure movements

--- src/test/java/org/example/BoardTest.java
+++ src/test/java/org/example/BoardTest.java
@@ -7,6 +7,9 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BoardTest {
@@ -138,7 +141,27 @@ public class BoardTest {
             // then
             assertThrows(IllegalArgumentException.class, () -> board.placeBlock(x, y));
         }
+    @Test
+    void checkPositionCollision() {
+        List<Position> possibleMoves = new ArrayList<>();
+        Figure figure = new Figure(4, 4, new Board(8));
 
+        possibleMoves.add(new Position(5, 5));
+        possibleMoves.add(new Position(6, 6));
+        possibleMoves.add(new Position(7, 7));
+        possibleMoves.add(new Position(5, 3));
+        possibleMoves.add(new Position(6, 2));
+        possibleMoves.add(new Position(7, 1));
+        possibleMoves.add(new Position(3, 5));
+        possibleMoves.add(new Position(2, 6));
+        possibleMoves.add(new Position(1, 7));
+        possibleMoves.add(new Position(3, 3));
+        possibleMoves.add(new Position(2, 2));
+        possibleMoves.add(new Position(1, 1));
+        possibleMoves.add(new Position(0, 0));
+
+        assertEquals(possibleMoves, figure.getPossibleMoves());
+    }
         @Test
         @DisplayName("Should throw exception when placing block on figure")
         void shouldThrowExceptionWhenPlacingBlockOnFigure() {
@@ -154,7 +177,84 @@ public class BoardTest {
             assertThrows(IllegalArgumentException.class, () -> board.placeBlock(x, y));
         }
     }
+    @Test
+    void checkPositionCollisionWithBlockage() {
+        List<Position> possibleMoves = new ArrayList<>();
+        Board board = new Board(8);
 
+        Figure figure = new Figure(4, 4, board);
+
+        board.placeBlock(6, 6);
+        board.placeBlock(6, 2);
+        board.placeBlock(0, 0);
+
+        possibleMoves.add(new Position(5, 5));
+        possibleMoves.add(new Position(6, 6));
+        possibleMoves.add(new Position(5, 3));
+        possibleMoves.add(new Position(6, 2));
+        possibleMoves.add(new Position(3, 5));
+        possibleMoves.add(new Position(2, 6));
+        possibleMoves.add(new Position(1, 7));
+        possibleMoves.add(new Position(3, 3));
+        possibleMoves.add(new Position(2, 2));
+        possibleMoves.add(new Position(1, 1));
+        possibleMoves.add(new Position(0, 0));
+
+        assertEquals(possibleMoves, figure.getPossibleMoves());
+
+    }
+        @Test
+    void checkPositionCollisionWithFigureAndBlockage() {
+        List<Position> possibleMoves1 = new ArrayList<>();
+        List<Position> possibleMoves2 = new ArrayList<>();
+
+        Board board = new Board(8);
+
+        Figure figure1 = new Figure(4, 4, board);
+        Figure figure2 = new Figure(6, 6, board);
+
+        board.placeBlock(6, 2);
+        board.placeBlock(0, 0);
+
+        possibleMoves1.add(new Position(5, 5));
+        possibleMoves1.add(new Position(6, 6));
+        possibleMoves1.add(new Position(5, 3));
+        possibleMoves1.add(new Position(6, 2));
+        possibleMoves1.add(new Position(3, 5));
+        possibleMoves1.add(new Position(2, 6));
+        possibleMoves1.add(new Position(1, 7));
+        possibleMoves1.add(new Position(3, 3));
+        possibleMoves1.add(new Position(2, 2));
+        possibleMoves1.add(new Position(1, 1));
+        possibleMoves1.add(new Position(0, 0));
+
+        possibleMoves2.add(new Position(7, 7));
+        possibleMoves2.add(new Position(7, 5));
+        possibleMoves2.add(new Position(5, 7));
+        possibleMoves2.add(new Position(5, 5));
+        possibleMoves2.add(new Position(4, 4));
+
+        assertEquals(possibleMoves1, figure1.getPossibleMoves());
+        assertEquals(possibleMoves2, figure2.getPossibleMoves());
+
+    }
+        @Test
+    void checkCollisionInLeftUpperCorner() {
+        List<Position> possibleMoves = new ArrayList<>();
+        Board board = new Board(8);
+
+        Figure figure = new Figure(0, 0, board);
+
+        possibleMoves.add(new Position(1, 1));
+        possibleMoves.add(new Position(2, 2));
+        possibleMoves.add(new Position(3, 3));
+        possibleMoves.add(new Position(4, 4));
+        possibleMoves.add(new Position(5, 5));
+        possibleMoves.add(new Position(6, 6));
+        possibleMoves.add(new Position(7, 7));
+
+        assertEquals(possibleMoves, figure.getPossibleMoves());
+    }
     @Nested
     @DisplayName("Complex Board Scenarios")
     class ComplexBoardScenarios {
