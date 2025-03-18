a6216fb4d460aec325fb625431aa242b77a6b115
Bond <rafal.majewski92@gmail.com>
2025-03-18 22:53:52 +0100

    refactor(Board): refactored to use enums

--- src/main/java/org/example/Board.java
+++ src/main/java/org/example/Board.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class Board {
     private final int size;
-    private final int [][] board;
+    private final BoardEnum [][] board;
 
     public Board (int size) {
 
@@ -13,10 +13,10 @@ public class Board {
         }
 
         this.size = size;
-        this.board = new int[size][size];
+        this.board = new BoardEnum[size][size];
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                this.board[i][j] = 0;
+                this.board[i][j] = BoardEnum.EMPTY;
             }
         }
     }
@@ -31,34 +31,34 @@ public class Board {
     public void checkPositionCollision(int x,int y){
        checkOutOfBounds(x,y);
 
-        if (this.board[x][y] > 0) {
+        if (this.board[x][y] != BoardEnum.EMPTY) {
             throw new IllegalArgumentException("Position is already occupied");
         }
     }
 
     public void placeFigure(int x, int y) {
         checkOutOfBounds(x,y);
-        this.board[x][y] = 1;
+        this.board[x][y] = BoardEnum.FIGURE;
     }
 
     public void placeBlock(int x, int y) {
         checkPositionCollision(x, y);
-        this.board[x][y] = 2;
+        this.board[x][y] = BoardEnum.BLOCK;
     }
 
     public boolean isOccupied(int x, int y) {
         checkOutOfBounds(x,y);
-        return this.board[x][y] != 0;
+        return this.board[x][y] != BoardEnum.EMPTY;
     }
 
     public boolean isFigure(int x, int y) {
         checkOutOfBounds(x,y);
-        return this.board[x][y] == 1;
+        return this.board[x][y] == BoardEnum.FIGURE;
     }
 
     public boolean isBlock(int x, int y) {
         checkOutOfBounds(x,y);
-        return this.board[x][y] == 2;
+        return this.board[x][y] == BoardEnum.BLOCK;
     }
 
     public int getSize() {
